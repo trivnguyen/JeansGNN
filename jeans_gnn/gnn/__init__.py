d5d98d485a19b20197e69f7cf7c7299f55974af8
Tri Nguyen <tnguy@mit.edu>
2023-04-13 22:34:32 -0400

    cleanup old functions

--- jeans_gnn/gnn/__init__.py
+++ jeans_gnn/gnn/__init__.py
@@ -1,13 +1,2 @@
 
-from . import modules, graph_regressors
-
-def create_graph_regression_module(config):
-    """ Create a graph regressor module from config file """
-
-    model = modules.MAFModule(
-        model=graph_regressors.GraphRegressor,
-        transform=None,
-        model_hparams=config["model"]["parameters"],
-        transform_hparams=config["transform"]["parameters"],
-        optimizer_hparams=config["optimizer"]["parameters"]
-    )
+from . import flows, graph_regressors, modules, transforms
