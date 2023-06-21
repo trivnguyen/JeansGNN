correct OM distribution; fix bug in cumulative mass calculation

src/jeans_gnn/im/gnn_im.py
@@ -177,7 +177,8 @@ class GNNInferenceModel():
                 checkpoint_path=checkpoint,
                 model_hparams=self.model_params,
                 optimizer_hparams=self.optimizer_params,
-                scheduler_hparams=self.scheduler_params
+                scheduler_hparams=self.scheduler_params,
+                map_location=torch.device('cpu') if not torch.cuda.is_available() else None,
             )
         self.transform = transforms.PhaseSpaceGraphProcessor(
             **self.transform_params)

src/jeans_gnn/jeans/density_profiles.py
@@ -32,12 +32,19 @@ class DensityProfile:
         # check if the array r is equally spaced
         dr = r[1] - r[0]
         if np.any(np.abs(np.diff(r) - dr) > 1e-6):
-            return integrate.cumtrapz(self.density(r), dx=dr, initial=0)
-        return integrate.cumtrapz(self.density(r), r, initial=0)
+            cmass = integrate.cumulative_trapezoid(
+                4 * np.pi * r**2 * self.density(r), dx=dr)
+        else:
+            cmass = integrate.cumulative_trapezoid(
+                4 * np.pi * r**2 * self.density(r), r)
+        cmass = np.append(cmass, cmass[-1])
+        return cmass
+
 
     def log_density(self, r: np.ndarray) -> np.ndarray:
         """ Compute the log10 density profile """
         raise NotImplementedError
+        
 
 class GeneralizedNFW(DensityProfile):
     """ Generalized NFW profile """